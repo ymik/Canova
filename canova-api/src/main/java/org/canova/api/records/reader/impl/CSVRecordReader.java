c35f8a7af220c39851e6da2e6cb58bc039799c47
Adam Gibson <adam@skymind.io>
2015-03-06 13:11:41 -0500

    add configuration for csv

--- canova-api/src/main/java/org/canova/api/records/reader/impl/CSVRecordReader.java
+++ canova-api/src/main/java/org/canova/api/records/reader/impl/CSVRecordReader.java
@@ -15,16 +15,50 @@ import java.util.List;
  * @author Adam Gibson
  */
 public class CSVRecordReader extends LineRecordReader {
+    private boolean skippedLines = false;
+    private int skipNumLines = 0;
+    private String delimiter = ",";
 
+    /**
+     * Skip first n lines
+     * @param skipNumLines the number of lines to skip
+     */
+    public CSVRecordReader(int skipNumLines) {
+         this(skipNumLines,",");
+    }
+
+    /**
+     * Skip lines and use delimiter
+     * @param skipNumLines the number of lines to skip
+     * @param delimiter the delimiter
+     */
+    public CSVRecordReader(int skipNumLines,String delimiter) {
+        this.skipNumLines = skipNumLines;
+        this.delimiter = delimiter;
+    }
+
+    public CSVRecordReader() {
+        this(0,",");
+    }
 
     @Override
     public Collection<Writable> next() {
+        if(!skippedLines && skipNumLines > 0) {
+            for(int i = 0; i < skipNumLines; i++) {
+                if(!hasNext()) {
+                    return new ArrayList<>();
+                }
+                super.next();
+            }
+            skippedLines = true;
+        }
         Text t =  (Text) super.next().iterator().next();
         String val = new String(t.getBytes());
-        String[] split = val.split(",");
+        String[] split = val.split(delimiter);
         List<Writable> ret = new ArrayList<>();
         for(String s : split)
             ret.add(new Text(s));
         return ret;
+
     }
 }

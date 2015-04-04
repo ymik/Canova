ac809e828e7b1118efd8235e0cd8cf89fc6a842a
Josh Patterson <jpatterson@floe.tv>
2015-04-04 00:32:59 -0400

    moving the deck chairs around

--- canova-cli/src/main/java/org/canova/cli/csv/transforms/Transforms.java
+++ canova-cli/src/main/java/org/canova/cli/csv/transforms/Transforms.java
@@ -11,14 +11,29 @@ public class Transforms {
 		throw new UnsupportedOperationException();
 	}
 
+	/*
+	 * Needed Statistics for binarize()
+	 * - range of values (min, max)
+	 * - similar to normalize, but we threshold on 0.5 after normalize
+	 */
         public double binarize(String inputColumnValue) {
         	throw new UnsupportedOperationException();
         }
 
+    	/*
+    	 * Needed Statistics for normalize()
+    	 * - range of values (min, max)
+    	 * - 
+    	 */        
         public double normalize(String inputColumnValue) {
         	throw new UnsupportedOperationException();
         }
 
+    	/*
+    	 * Needed Statistics for label()
+    	 * - count of distinct labels
+    	 * - index of labels to IDs (hashtable?)
+    	 */                
         public double label(String inputColumnValue) {
         	throw new UnsupportedOperationException();
         }

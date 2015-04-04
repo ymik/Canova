d74289be72e062e4d6a6b4d6a142754532786f5b
Josh Patterson <jpatterson@floe.tv>
2015-04-03 23:40:28 -0400

    added the basic mechanics for tracking min/max in the column stats, and label counts for class balancing

--- canova-cli/src/main/java/org/canova/cli/csv/schema/CSVSchemaColumn.java
+++ canova-cli/src/main/java/org/canova/cli/csv/schema/CSVSchemaColumn.java
@@ -39,10 +39,10 @@ public class CSVSchemaColumn {
 	 * - how do we model statistics per column?
 	 * 
 	 */
-	public double minValue = 0;
-	public double maxValue = 0;
-	public double stddev = 0;
-	public double median = 0;
+	public double minValue = Double.NaN;
+	public double maxValue = Double.NaN;
+	//public double stddev = 0;
+	//public double median = 0;
 	
 	// used to track input values that do not match the schema data type
 	public long invalidDataEntries = 0;
@@ -52,6 +52,15 @@ public class CSVSchemaColumn {
 	// we want to track the label counts to understand the class balance
 	private Map<String, Integer> recordLabels = new LinkedHashMap<String, Integer>();
 	
+	
+	public CSVSchemaColumn(String colName, ColumnType colType, TransformType transformType) {
+		
+		this.name = colName;
+		this.columnType = colType;
+		this.transform = transformType;
+		
+	}
+	
 	/**
 	 * This method collects dataset statistics about the column that we'll 
 	 * need later to
@@ -59,18 +68,96 @@ public class CSVSchemaColumn {
 	 * 2. report on column specfic statistics to give visibility into the properties of the input dataset
 	 * 
 	 * @param value
+	 * @throws Exception 
 	 */
-	public void evaluateColumnValue(String value) {
+	public void evaluateColumnValue(String value) throws Exception {
 		
 		if ( ColumnType.NUMERIC == this.columnType ) {
 			
+			// then we want to look at min/max values
 			
-		} else if ( ColumnType.STRING == this.columnType ) {
+			double tmpVal = Double.parseDouble(value);
+			
+			System.out.println( "converted: " + tmpVal );
+			
+			if (Double.isNaN(tmpVal)) {
+				throw new Exception("The column was defined as Numeric yet could not be parsed as a Double");
+			}
+			
+			if ( Double.isNaN( this.minValue ) ) {
+			
+				this.minValue = tmpVal;
+				
+			} else if (tmpVal < this.minValue) {
+				
+				this.minValue = tmpVal;
+				
+			}
+			
+			if ( Double.isNaN( this.maxValue ) ) {
+				
+				this.maxValue = tmpVal;
+				
+			} else if (tmpVal > this.maxValue) {
+				
+				this.maxValue = tmpVal;
+				
+			}
+			
+		} else if ( TransformType.LABEL == this.transform ) {
+			
+			
+			
+			// then we want to track the record label
+			if ( this.recordLabels.containsKey( value ) ) {
+				
+				Integer countInt = this.recordLabels.get( value );
+				countInt++;
+				this.recordLabels.put( value, countInt );
+				
+			} else {
+				
+				this.recordLabels.put( value, 1 );
+				
+			}
+			
+		}
+		
+	}
+	
+	public void computeStatistics() {
+		
+		if ( ColumnType.NUMERIC == this.columnType ) {
+			
+		//} else if ( Column == this.columnType ) {
+			
+		} else {
 			
 			
 		}
 		
 	}
+	
+	public void debugPrintColumns() {
+		
+		for (Map.Entry<String, Integer> entry : this.recordLabels.entrySet()) {
+		    
+			String key = entry.getKey();
+		    Integer value = entry.getValue();
+		    
+		    System.out.println( "> " + key + ", " + value);
+		    
+		    // now work with key and value...
+		}		
+		
+	}
+	
+	public Integer getLabelCount( String label ) {
+				
+		return this.recordLabels.get( label );
+		
+	}
+	
 	
 
 }
--- canova-cli/src/test/java/org/canova/cli/csv/schema/TestCSVSchemaColumn.java
+++ canova-cli/src/test/java/org/canova/cli/csv/schema/TestCSVSchemaColumn.java
@@ -7,13 +7,66 @@ import org.junit.Test;
 public class TestCSVSchemaColumn {
 
 	@Test
-	public void testClassBalanceReporting() {
-		//fail("Not yet implemented");
+	public void testClassBalanceReporting() throws Exception {
+
+		CSVSchemaColumn schemaCol_0 = new CSVSchemaColumn( "a", CSVSchemaColumn.ColumnType.STRING, CSVSchemaColumn.TransformType.LABEL );
+	
+		schemaCol_0.evaluateColumnValue("alpha");
+		schemaCol_0.evaluateColumnValue("beta");
+		schemaCol_0.evaluateColumnValue("gamma");
+
+		schemaCol_0.evaluateColumnValue("alpha");
+		schemaCol_0.evaluateColumnValue("beta");
+		schemaCol_0.evaluateColumnValue("gamma");
+
+		schemaCol_0.evaluateColumnValue("alpha");
+		schemaCol_0.evaluateColumnValue("beta");
+
+		schemaCol_0.evaluateColumnValue("alpha");
+		
+		schemaCol_0.debugPrintColumns();
+		
+		assertEquals( 3, schemaCol_0.getLabelCount("beta"), 0.0 );
+		assertEquals( 2, schemaCol_0.getLabelCount("gamma"), 0.0 );
+		assertEquals( 4, schemaCol_0.getLabelCount("alpha"), 0.0 );
+
 	}
 
 	@Test
-	public void testMinMaxMetrics() {
-		//fail("Not yet implemented");
+	public void testMinMaxMetrics() throws Exception {
+		
+		CSVSchemaColumn schemaCol_0 = new CSVSchemaColumn( "a", CSVSchemaColumn.ColumnType.NUMERIC, CSVSchemaColumn.TransformType.COPY );
+		
+		schemaCol_0.evaluateColumnValue("1");
+		schemaCol_0.evaluateColumnValue("2");
+		schemaCol_0.evaluateColumnValue("3");
+		
+		//schemaCol_0.computeStatistics();
+		
+		assertEquals( 1, schemaCol_0.minValue, 0.0 );
+		assertEquals( 3, schemaCol_0.maxValue, 0.0 );
+	}
+
+	@Test
+	public void testMinMaxMetricsMixedInsertOrder() throws Exception {
+		
+		CSVSchemaColumn schemaCol_0 = new CSVSchemaColumn( "a", CSVSchemaColumn.ColumnType.NUMERIC, CSVSchemaColumn.TransformType.COPY );
+		
+		schemaCol_0.evaluateColumnValue("6");
+		schemaCol_0.evaluateColumnValue("-2");
+		schemaCol_0.evaluateColumnValue("3");
+		
+		//schemaCol_0.computeStatistics();
+		
+		assertEquals( -2, schemaCol_0.minValue, 0.0 );
+		assertEquals( 6, schemaCol_0.maxValue, 0.0 );
+	}
+	
+	@Test
+	public void testSchemaEvaluation() {
+		
+		// colSchemaEntry.evaluateColumnValue( columns[ colIndex ] );
+		
 	}
 	
 	

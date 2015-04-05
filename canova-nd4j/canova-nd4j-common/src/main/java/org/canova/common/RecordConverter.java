e6638fbaee63f58bc9116208ab0522eb62913474
Adam Gibson <adam@skymind.io>
2015-04-05 18:19:55 -0400

    update record converter

--- canova-nd4j/canova-nd4j-common/src/main/java/org/canova/common/RecordConverter.java
+++ canova-nd4j/canova-nd4j-common/src/main/java/org/canova/common/RecordConverter.java
@@ -5,9 +5,11 @@ import org.canova.api.io.data.FloatWritable;
 import org.canova.api.writable.Writable;
 import org.nd4j.linalg.api.buffer.DataBuffer;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * @author Adam Gibson
@@ -15,7 +17,21 @@ import java.util.Collection;
 public class RecordConverter {
     private RecordConverter() {}
 
+    /**
+     * Convert an ndarray to a record
+     * @param record the array to convert
+     * @return the record
+     */
+    public static INDArray toRecord(Collection<Writable> record) {
+        Iterator<Writable> writables = record.iterator();
+        INDArray linear = Nd4j.zeros(record.size());
 
+        int count = 0;
+        while(writables.hasNext()) {
+            linear.putScalar(count++,Double.valueOf(writables.next().toString()));
+        }
+        return linear;
+    }
     /**
      * Convert an ndarray to a record
      * @param array the array to convert

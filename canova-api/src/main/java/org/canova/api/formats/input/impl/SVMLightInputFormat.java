0f1f3c4b975bd6b8fc39520e8c0752863f504a72
Adam Gibson <adam@skymind.io>
2015-05-05 21:05:14 -0700

    add record readers

--- canova-api/src/main/java/org/canova/api/formats/input/impl/SVMLightInputFormat.java
+++ canova-api/src/main/java/org/canova/api/formats/input/impl/SVMLightInputFormat.java
@@ -1,20 +1,46 @@
+/*
+ *
+ *  *
+ *  *  * Copyright 2015 Skymind,Inc.
+ *  *  *
+ *  *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  *    you may not use this file except in compliance with the License.
+ *  *  *    You may obtain a copy of the License at
+ *  *  *
+ *  *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *  *
+ *  *  *    Unless required by applicable law or agreed to in writing, software
+ *  *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  *    See the License for the specific language governing permissions and
+ *  *  *    limitations under the License.
+ *  *
+ *
+ */
+
 package org.canova.api.formats.input.impl;
 
 
 
-import org.canova.api.formats.input.InputFormat;
+import org.canova.api.conf.Configuration;
+import org.canova.api.formats.input.BaseInputFormat;
 import org.canova.api.records.reader.RecordReader;
 import org.canova.api.records.reader.impl.SVMLightRecordReader;
 import org.canova.api.split.InputSplit;
 
-import java.io.DataInput;
-import java.io.DataOutput;
 import java.io.IOException;
 
 /**
- * Created by agibsonccc on 1/11/15.
+ * SVMLight input format
+ *
+ * @author Adam Gibson
  */
-public class SVMLightInputFormat implements InputFormat {
+public class SVMLightInputFormat extends BaseInputFormat {
+    @Override
+    public RecordReader createReader(InputSplit split, Configuration conf) throws IOException, InterruptedException {
+        return createReader(split);
+    }
+
     @Override
     public RecordReader createReader(InputSplit split) throws IOException, InterruptedException {
         SVMLightRecordReader reader = new SVMLightRecordReader();
@@ -22,13 +48,5 @@ public class SVMLightInputFormat implements InputFormat {
         return reader;
     }
 
-    @Override
-    public void write(DataOutput out) throws IOException {
 
-    }
-
-    @Override
-    public void readFields(DataInput in) throws IOException {
-
-    }
 }

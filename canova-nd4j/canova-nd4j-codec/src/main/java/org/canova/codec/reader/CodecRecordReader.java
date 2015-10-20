ed29fad13d037bd47e0db1835953928ef7f8db04
Alex Black <blacka101@gmail.com>
2015-10-20 23:49:29 +1100

    Add CSVSequenceRecordReader

--- canova-nd4j/canova-nd4j-codec/src/main/java/org/canova/codec/reader/CodecRecordReader.java
+++ canova-nd4j/canova-nd4j-codec/src/main/java/org/canova/codec/reader/CodecRecordReader.java
@@ -54,6 +54,7 @@ import java.util.Collection;
  * @author Adam Gibson
  */
 public class CodecRecordReader extends FileRecordReader implements SequenceRecordReader {
+    private int startFrame = 0;
     private int numFrames = -1;
     private int totalFrames = -1;
     private double framesPerSecond = -1;
@@ -76,7 +77,7 @@ public class CodecRecordReader extends FileRecordReader implements SequenceRecor
         File next = iter.next();
         Collection<Collection<Writable>> record = new ArrayList<>();
         if(numFrames >= 1) {
-            for(int i = numFrames; i < totalFrames; i++) {
+            for(int i = startFrame; i < startFrame+numFrames; i++) {
                 try {
                     BufferedImage grab = FrameGrab.getFrame(next,i + 1);
                     if(ravel)
@@ -138,7 +139,8 @@ public class CodecRecordReader extends FileRecordReader implements SequenceRecor
     @Override
     public void setConf(Configuration conf) {
         super.setConf(conf);
-        numFrames = conf.getInt(START_FRAME,-1);
+        startFrame = conf.getInt(START_FRAME,0);
+        numFrames = conf.getInt(TOTAL_FRAMES,-1);
         int rows = conf.getInt(ROWS,28);
         int cols = conf.getInt(COLUMNS,28);
         imageLoader = new ImageLoader(rows,cols);

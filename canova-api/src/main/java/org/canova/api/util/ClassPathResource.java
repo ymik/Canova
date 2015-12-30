dd50b8e802cbf6cc68b7f98938565a7db3341728
raver119 <raver119@gmail.com>
2015-12-30 17:23:58 +0300

    ClassPathResource starting slash fix

--- canova-api/src/main/java/org/canova/api/util/ClassPathResource.java
+++ canova-api/src/main/java/org/canova/api/util/ClassPathResource.java
@@ -54,8 +54,14 @@ public class ClassPathResource {
         URL url = loader.getResource(this.resourceName);
         if (url == null) {
             // try to check for mis-used starting slash
+            // TODO: see TODO below
             if (this.resourceName.startsWith("/")) {
                 url = loader.getResource(this.resourceName.replaceFirst("[\\\\/]",""));
+                if (url != null) return url;
+            } else {
+                // try to add slash, to make clear it's not an issue
+                // TODO: change this mechanic to actual path purifier
+                url = loader.getResource("/" + this.resourceName);
                 if (url != null) return url;
             }
             throw new IllegalStateException("Resource '" + this.resourceName + "' cannot be found.");

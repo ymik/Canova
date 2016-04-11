8a734791051ab5901deb8934156225731b481c3c
Alex Black <blacka101@gmail.com>
2016-04-11 19:19:23 +1000

    Additional test for JacksonRecordReader (appending labels)

--- canova-api/src/test/java/org/canova/api/records/reader/impl/JacksonRecordReaderTest.java
+++ canova-api/src/test/java/org/canova/api/records/reader/impl/JacksonRecordReaderTest.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.canova.api.io.data.DoubleWritable;
+import org.canova.api.io.data.IntWritable;
 import org.canova.api.io.data.Text;
+import org.canova.api.io.labels.PathLabelGenerator;
 import org.canova.api.records.reader.RecordReader;
 import org.canova.api.records.reader.impl.jackson.FieldSelection;
 import org.canova.api.records.reader.impl.jackson.JacksonRecordReader;
@@ -14,6 +17,7 @@ import org.canova.api.util.ClassPathResource;
 import org.canova.api.writable.Writable;
 import org.junit.Test;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -109,6 +113,57 @@ public class JacksonRecordReaderTest {
         assertEquals(exp1, rr.next());
         assertEquals(exp2, rr.next());
         assertFalse(rr.hasNext());
+    }
+
+    @Test
+    public void testAppendingLabels() throws Exception {
+        ClassPathResource cpr = new ClassPathResource("json/json_test_0.txt");
+        String path = cpr.getFile().getAbsolutePath().replace("0", "%d");
+
+        InputSplit is = new NumberedFileInputSplit(path, 0, 2);
+
+        //Insert at the end:
+        RecordReader rr = new JacksonRecordReader(getFieldSelection(), new ObjectMapper(new JsonFactory()),
+                false, -1, new LabelGen());
+        rr.initialize(is);
+
+        List<Writable> exp0 = Arrays.asList((Writable)new Text("aValue0"), new Text("bValue0"), new Text("cxValue0"), new IntWritable(0));
+        assertEquals(exp0, rr.next());
+
+        List<Writable> exp1 = Arrays.asList((Writable)new Text("aValue1"), new Text("MISSING_B"), new Text("cxValue1"), new IntWritable(1));
+        assertEquals(exp1, rr.next());
+
+        List<Writable> exp2 = Arrays.asList((Writable)new Text("aValue2"), new Text("bValue2"), new Text("MISSING_CX"), new IntWritable(2));
+        assertEquals(exp2, rr.next());
+
+        //Insert at position 0:
+        rr = new JacksonRecordReader(getFieldSelection(), new ObjectMapper(new JsonFactory()),
+                false, -1, new LabelGen(), 0);
+        rr.initialize(is);
+
+        exp0 = Arrays.asList((Writable)new IntWritable(0), new Text("aValue0"), new Text("bValue0"), new Text("cxValue0"));
+        assertEquals(exp0, rr.next());
+
+        exp1 = Arrays.asList((Writable)new IntWritable(1), new Text("aValue1"), new Text("MISSING_B"), new Text("cxValue1"));
+        assertEquals(exp1, rr.next());
+
+        exp2 = Arrays.asList((Writable)new IntWritable(2), new Text("aValue2"), new Text("bValue2"), new Text("MISSING_CX"));
+        assertEquals(exp2, rr.next());
+    }
+
+    private static class LabelGen implements PathLabelGenerator {
+
+        @Override
+        public Writable getLabelForPath(String path) {
+            if(path.endsWith("0.txt")) return new IntWritable(0);
+            else if(path.endsWith("1.txt")) return new IntWritable(1);
+            else return new IntWritable(2);
+        }
+
+        @Override
+        public Writable getLabelForPath(URI uri) {
+            return getLabelForPath(uri.getPath());
+        }
     }
 
 }
